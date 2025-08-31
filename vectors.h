c75c43668db4bf29e979b31e0450716f88d630c8
jwDel1 <jacksonwaldmang@gmail.com>
2025-08-31 09:50:02 -0700

    header file for vectors lib

---
+++ vectors.h
@@ -0,0 +1,10 @@
+#ifndef VECTORS_H
+#define VECTORS_H
+
+void sub_Vec(Vec2, Vec2);
+void add_Vec(Vec2, Vec2);
+void norm_Vec(Vec2);
+float dot_Prod(Vec2, Vec2);
+void cross_Prod(Vec2, Vec2);
+
+#endif
